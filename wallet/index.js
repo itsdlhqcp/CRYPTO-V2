bdcd2cce25bc90df4fbfe560d417bbb8c89e2fce
DILHAQUE <dilhaqued@gmail.com>
2022-10-20 23:07:58 +0530

    signature validataed

--- wallet/index.js
+++ wallet/index.js
@@ -13,16 +13,20 @@
 // module.exports = Wallet;
 const { STARTING_BALANCE } = require('../config');
 const { ec } = require('../util');
+const cryptoHash = require('../util/crypto-hash');
 
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE;
 
-    const keyPair = ec.genKeyPair();
+    this.keyPair = ec.genKeyPair();
 
-    this.publicKey = keyPair.getPublic().encode('hex');
+    this.publicKey = this.keyPair.getPublic().encode('hex');
   }
-}
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data));
+  }
+};
 
 // module.exports = Wallet;
 module.exports = Wallet;
