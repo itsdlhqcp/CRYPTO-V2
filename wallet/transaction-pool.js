40d83771fc2e3e02a810532234f9e126af73d955
DILHAQUE <dilhaqued@gmail.com>
2022-10-22 14:10:26 +0530

    clear blockchain trasaction

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -28,6 +28,11 @@ class TransactionPool {
     constructor() {
       this.transactionMap = {};
     }
+
+    clear() {
+        this.transactionMap = {};
+      }
+
     setTransaction(transaction) {
       this.transactionMap[transaction.id] = transaction;
     }
@@ -46,6 +51,18 @@ class TransactionPool {
         return Object.values(this.transactionMap).filter(
           transaction => Transaction.validTransaction(transaction)
         );
+      }
+
+      clearBlockchainTransactions({ chain }) {
+        for (let i=1; i<chain.length; i++) {
+          const block = chain[i];
+    
+          for (let transaction of block.data) {
+            if (this.transactionMap[transaction.id]) {
+              delete this.transactionMap[transaction.id];
+            }
+          }
+        }
       }
   }
   module.exports = TransactionPool;
