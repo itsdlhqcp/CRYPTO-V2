a3dde40d7e9e618c75f2057644b2e27d602a34c3
DILHAQUE <dilhaqued@gmail.com>
2022-10-22 14:31:07 +0530

    calculate walllet balence before each trasaction

--- wallet/index.js
+++ wallet/index.js
@@ -27,7 +27,14 @@ class Wallet {
    sign(data) {
     return this.keyPair.sign(cryptoHash(data));
   }
-  createTransaction({ recipient, amount }) {
+  // createTransaction({ recipient, amount }) {
+    createTransaction({ recipient, amount, chain }) {
+      if (chain) {
+        this.balance = Wallet.calculateBalance({
+          chain,
+          address: this.publicKey
+        });
+      }
     if (amount > this.balance) {
       throw new Error('Amount exceeds balance');
     }
