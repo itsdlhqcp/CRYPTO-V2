c7803bee8637b3bfa1e91d2062fe693e976ec346
DILHAQUE <dilhaqued@gmail.com>
2022-10-22 11:28:37 +0530

    API transaction

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -19,15 +23,26 @@ app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
 });
 
-app.post('/api/mine', (req, res) => {
-    const { data } = req.body;
+// app.post('/api/mine', (req, res) => {
+//     const { data } = req.body;
   
-    blockchain.addBlock({ data });
+//     blockchain.addBlock({ data });
 
-    pubsub.broadcastChain();
+//     pubsub.broadcastChain();
   
-    res.redirect('/api/blocks');
-  });
+//     res.redirect('/api/blocks');
+//   });
+app.post('/api/transact', (req, res) => {
+  const { amount, recipient } = req.body;
+
+  const transaction = wallet.createTransaction({ recipient, amount });
+
+  transactionPool.setTransaction(transaction);
+
+  console.log('transactionPool', transactionPool);
+
+  res.json({ transaction });
+});
 
   const syncChains = () => {
     request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
