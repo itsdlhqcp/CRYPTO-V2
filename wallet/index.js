bb0433fed832c31e3e8194162b36712f7da5c1f3
DILHAQUE <dilhaqued@gmail.com>
2022-10-22 10:19:25 +0530

    wallet create  transaction

--- wallet/index.js
+++ wallet/index.js
@@ -11,6 +11,7 @@
 // };
 
 // module.exports = Wallet;
+const Transaction = require('./transaction');
 const { STARTING_BALANCE } = require('../config');
 const { ec } = require('../util');
 const cryptoHash = require('../util/crypto-hash');
@@ -25,6 +26,13 @@ class Wallet {
   }
    sign(data) {
     return this.keyPair.sign(cryptoHash(data));
+  }
+  createTransaction({ recipient, amount }) {
+    if (amount > this.balance) {
+      throw new Error('Amount exceeds balance');
+    }
+
+    return new Transaction({ senderWallet: this, recipient, amount });
   }
 };
 
