745c863540d69f8d92d22772466f3d1dd5ece363
DILHAQUE <dilhaqued@gmail.com>
2022-10-24 22:05:31 +0530

    app component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 render(
-    <div>Cryptochain in React! Live update</div>,
+    // <div>Cryptochain in React! Live update</div>,
+    <App/>,
     document.getElementById('root')
   );
