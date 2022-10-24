f1471602a7d082e4457e8cec26116a415333bf6e
DILHAQUE <dilhaqued@gmail.com>
2022-10-24 23:01:06 +0530

    parsel error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,18 @@
+import { response } from "express";
 import React, { Component } from "react";
 
 class App extends Component {
+    state = { walletInfo: { address: 'fooxv6', balance: 9999}};
+    componentDidMount() {
+        fetch('http://localhost:3000/api/wallet-info').then(response =>console.log('response',response));
+    }
     render () {
+        const { address, balance} =this.state.walletInfo;
         return(
             <div>
-                Welcome to the blockchain..
+                <div> Welcome to the blockchain...</div> 
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
             </div>
         )
     }
