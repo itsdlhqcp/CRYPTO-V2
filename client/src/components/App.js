0d7ae97c60b673fd29b7f645b7690b07786a4f5f
DILHAQUE <dilhaqued@gmail.com>
2022-10-25 12:54:01 +0530

    fetch and display wallet info

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,48 @@
-import { response } from "express";
-import React, { Component } from "react";
+// import { response } from "express";
+// import React, { Component } from "react";
+
+// class App extends Component {
+//     state = { walletInfo: { address: 'fooxv6', balance: 9999}};
+//     componentDidMount() {
+//         fetch('http://localhost:3000/api/wallet-info').then(response =>console.log('response',response));
+//     }
+//     render () {
+//         const { address, balance} =this.state.walletInfo;
+//         return(
+//             <div>
+//                 <div> Welcome to the blockchain...</div> 
+//                 <div>Address: {address}</div>
+//                 <div>Balance: {balance}</div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react';
 
 class App extends Component {
-    state = { walletInfo: { address: 'fooxv6', balance: 9999}};
-    componentDidMount() {
-        fetch('http://localhost:3000/api/wallet-info').then(response =>console.log('response',response));
-    }
-    render () {
-        const { address, balance} =this.state.walletInfo;
-        return(
-            <div>
-                <div> Welcome to the blockchain...</div> 
-                <div>Address: {address}</div>
-                <div>Balance: {balance}</div>
-            </div>
-        )
-    }
-}
+  state = { walletInfo: {} };
 
+  componentDidMount() {
+    fetch('http://localhost:3000/api/wallet-info')
+      .then(response => response.json())
+      .then(json => this.setState({ walletInfo: json }));
+  }
+
+  render() {
+    const { address, balance } = this.state.walletInfo;
+
+    return (
+      <div>
+       
+        <div>Welcome to the blockchain...</div>
+        <div>Address: {address}</div>
+        <div>Balance: {balance}</div>
+      </div>
+    );
+  }
+}
 export default App;
