f3f0642b15a95f468c63a40979d719139feaf171
DILHAQUE <dilhaqued@gmail.com>
2022-10-26 21:39:25 +0530

    visulize block code @404

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,34 @@
 
+// import React, { Component } from 'react';
+// import Blocks from './Blocks';
+
+// class App extends Component {
+//   state = { walletInfo: {} };
+
+//   componentDidMount() {
+//     fetch('http://localhost:3000/api/wallet-info')
+//       .then(response => response.json())
+//       .then(json => this.setState({ walletInfo: json }));
+//   }
+
+//   render() {
+//     const { address, balance } = this.state.walletInfo;
+
+//     return (
+//       <div>
+       
+//         <div>Welcome to the blockchain...</div>
+//         <div>Address: {address}</div>
+//         <div>Balance: {balance}</div>
+//         <br />
+//         <Blocks />
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+
 import React, { Component } from 'react';
-import Blocks from './Blocks';
 
 class App extends Component {
   state = { walletInfo: {} };
@@ -16,12 +44,10 @@ class App extends Component {
 
     return (
       <div>
-       
+        Welcome to the blockchain...
         <div>Welcome to the blockchain...</div>
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
-        <br />
-        <Blocks />
       </div>
     );
   }
